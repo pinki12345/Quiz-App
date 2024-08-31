9a2acd64738db53226960edda4ecfb40bf27c29d
Pinki <pinkichaudhary749@gmail.com>
2024-08-31 18:31:31 +0530

    All Changes are done!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import PollFeedback from "./components/PollFeedback";
+import QuizAnalysis from "./components/QuizAnalysis";
+import QuizFeedback from "./components/QuizFeedback";
+import QuizInterface from "./components/QuizInterface ";
+import QuizQuestionAnalysis from "./components/QuizQuestionAnalysis";
+import Error from "./pages/Error";
+import SignupLogin from "./pages/SignupLogin";
+import PrivateRoute from "./components/PrivateRoute";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/auth" />} />
+        <Route path="/auth" element={<SignupLogin />} />
+
+        <Route
+          path="/home/*"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        >
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="analytics" element={<QuizAnalysis />} />
+          <Route
+            path="quizQuestionAnalysis/:quizId"
+            element={<QuizQuestionAnalysis />}
+          />
+        </Route>
+        <Route path="/quizInterface/:id" element={<QuizInterface />} />
+        <Route path="/feedback" element={<QuizFeedback />} />
+        <Route path="/pollFeedback" element={<PollFeedback />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
